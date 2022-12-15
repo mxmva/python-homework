e80590b4c1b9557aa7a9fb0941833850ae07a844
Юлия Максимова <yliyamaksimova@MacBook-Pro-Ulia.local>
2022-12-15 22:23:31 +0300

    Добавили третью задачу

---
+++ task_3.py
@@ -0,0 +1,12 @@
+x = int(input('x = '))
+y = int (input('y = '))
+if x>0 and y>0:
+    print('I')
+elif x<0 and y>0:
+    print('II')
+elif x<0 and y<0:
+    print('III')
+elif x>0 and y<0:
+    print('IV')
+else:
+    print('Число должно быть больше нуля')
