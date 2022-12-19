ae55b58e5449de83fe2802dc7a3a9405a42b1883
Юлия Максимова <yliyamaksimova@MacBook-Pro-Ulia.local>
2022-12-19 20:19:24 +0300

    Добавили четвертую и пятцю задачи

---
+++ task_4.py
@@ -0,0 +1,10 @@
+nomerChetverti = [1,2,3,4]
+chetvert = int (input('Please entre to number '))
+if chetvert == nomerChetverti[0]:
+    print ('x>0 y>0')
+elif nomerChetverti [1]:
+    print ('x<0 y>0')
+elif nomerChetverti [2]:
+    print ('x<0 y<0')
+elif nomerChetverti [3]:
+    print ('x>0 y<0')
---
+++ task_5.py
@@ -0,0 +1,7 @@
+
+x1 = int(input('Введите x1 = '))
+y1 = int(input('Введите y1 = '))
+x2 = int(input('Введите x2 = '))
+y2 = int(input('Введите y2 = '))
+result = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+print (result)
